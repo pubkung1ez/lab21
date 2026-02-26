029478db5850378d87f3f2c9400ae0b8cda77404
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-02-27 04:10:36 +0700

    suss

--- average.cpp
+++ average.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc == 1) {
+        cout << "Please input numbers to find average.\n";
+        return 0;
+    }
+
+    int count = argc - 1;
+    double sum = 0.0;
+
+    for (int i = 1; i < argc; i++) {
+        sum += atof(argv[i]); 
+    }
+
+    double average = sum / count;
+
+    cout << "---------------------------------\n";
+    cout << "Average of " << count << " numbers = " << average << "\n";
+    cout << "---------------------------------\n";
+
+    return 0;
+}
